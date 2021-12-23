2f54634084b1bfdbdf85769e1343fe9467cabab7
Sean-Coll <c18332623@mytudublin.ie>
2021-12-23 21:31:07 +0000

    Added ResultActivity but no functionality or correct layout.

--- app/src/main/java/ie/tudublin/carml/ResultActivity.java
+++ app/src/main/java/ie/tudublin/carml/ResultActivity.java
@@ -1,26 +1,22 @@
 package ie.tudublin.carml;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PricePredictionActivity extends AppCompatActivity implements View.OnClickListener {
+public class ResultActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton back_arrow;
-    ImageButton submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.price_prediction_activity);
+        setContentView(R.layout.result_activity);
 
         back_arrow = findViewById(R.id.back_arrow);
         back_arrow.setOnClickListener(this);
-        submit = findViewById(R.id.submit_button);
-        submit.setOnClickListener(this);
     }
 
     @Override
@@ -28,11 +24,6 @@ public class PricePredictionActivity extends AppCompatActivity implements View.O
         switch(view.getId()) {
             case(R.id.back_arrow): {
                 finish();
-                break;
-            }
-            case(R.id.submit_button): {
-                Intent result = new Intent(PricePredictionActivity.this, ResultActivity.class);
-                startActivity(result);
                 break;
             }
             default: {
