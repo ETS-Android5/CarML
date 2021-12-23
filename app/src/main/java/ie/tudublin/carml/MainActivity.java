e8615bec90bf7614fa28dc5b4f6dbd783d4ea471
Sean-Coll <c18332623@mytudublin.ie>
2021-12-23 16:40:31 +0000

    Added PricePredictionActivity.java and price_prediction_activity.xml. Added in moving between those two activities but needs to be tested.

--- app/src/main/java/ie/tudublin/carml/MainActivity.java
+++ app/src/main/java/ie/tudublin/carml/MainActivity.java
@@ -2,13 +2,35 @@ package ie.tudublin.carml;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton price_prediction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        price_prediction = findViewById(R.id.price_prediction_button);
+        price_prediction.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case(R.id.price_prediction_button): {
+                Intent price_prediction = new Intent(MainActivity.this, PricePredictionActivity.class);
+                break;
+            }
+            default: {
+                break;
+            }
+        }
     }
 }
---
+++ app/src/main/java/ie/tudublin/carml/PricePredictionActivity.java
@@ -0,0 +1,34 @@
+package ie.tudublin.carml;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PricePredictionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton back_arrow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.price_prediction_activity);
+
+        back_arrow.findViewById(R.id.back_arrow);
+        back_arrow.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case(R.id.back_arrow): {
+                finish();
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+}
