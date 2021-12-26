cede5454bd0722a680cdd483bf73f85dc35b0089
Sean-Coll <c18332623@mytudublin.ie>
2021-12-26 21:26:48 +0000

    Added dropdown menus (Spinners) for manufacturer and model. Added NumberPicker for year. However, duplicates appear in manufacturer list and both lists need their appearance changed.

--- app/src/main/java/ie/tudublin/carml/PricePredictionActivity.java
+++ app/src/main/java/ie/tudublin/carml/PricePredictionActivity.java
@@ -3,14 +3,31 @@ package ie.tudublin.carml;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PricePredictionActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton back_arrow;
     ImageButton submit;
+    Spinner manufacturerDD;
+    Spinner modelDD;
+    NumberPicker year;
+    // Create ArrayLists that hold Manufacturer and Model
+    private List<String> manufacturers = new ArrayList<>();
+    private List<String> models = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,20 @@ public class PricePredictionActivity extends AppCompatActivity implements View.O
         back_arrow.setOnClickListener(this);
         submit = findViewById(R.id.submit_button);
         submit.setOnClickListener(this);
+        year = findViewById(R.id.year);
+        year.setMinValue(1990);
+        year.setMaxValue(2017);
+        prepSpinners();
+        manufacturerDD = findViewById(R.id.manufacturer);
+        modelDD = findViewById(R.id.model);
+        ArrayAdapter<String> manAdapter =
+                new ArrayAdapter<>(this, R.layout.spinner_item, manufacturers);
+        manAdapter.setDropDownViewResource(R.layout.spinner_item);
+        manufacturerDD.setAdapter(manAdapter);
+        ArrayAdapter<String> modAdapter =
+                new ArrayAdapter<>(this, R.layout.spinner_item, models);
+        modAdapter.setDropDownViewResource(R.layout.spinner_item);
+        modelDD.setAdapter(modAdapter);
     }
 
     @Override
@@ -42,7 +73,59 @@ public class PricePredictionActivity extends AppCompatActivity implements View.O
     }
 
     public void prepSpinners() {
+        // Add in the first value for the dropdown menus
+        manufacturers.add("Select One");
+        models.add("Select One");
 
+        // Get InputStream for data
+        InputStream inStream = getResources().openRawResource(R.raw.kaggle_data_clean);
+        // Create Buffered Reader for stream
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(inStream, StandardCharsets.UTF_8));
+
+        // i is used to check if the previous item in the list is the same as the current
+        int ma = 1;
+        int mo = 1;
+        String row = "f";
+        // Read each row and parse the correct parts into the correct lists
+        try {
+            // Skip over the column headers
+            reader.readLine();
+            while((row = reader.readLine()) != null)
+            {
+                // Split each row on the commas
+                String[] splits = row.split(",");
+
+                // Add in the first ones
+//                if (ma == 0) {
+//                    manufacturers.add(splits[0]);
+//                    ma++;
+//                }
+//
+//                if (mo == 0) {
+//                    models.add(splits[1]);
+//                    mo++;
+//                }
+
+                if (ma > 0) {
+                    if (!manufacturers.get(ma-1).equals(splits[0])) {
+                        manufacturers.add(splits[0]);
+                        ma++;
+                    }
+                }
+
+                if (mo > 0) {
+                    if (!models.get(mo-1).equals(splits[1])) {
+                        models.add(splits[1]);
+                        mo++;
+                    }
+                }
+            }
+
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 
     public void readColumn() {
