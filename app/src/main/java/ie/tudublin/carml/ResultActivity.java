c8d207e0ac425538e53967a7f041d41458563d80
Sean-Coll <c18332623@mytudublin.ie>
2022-01-01 21:50:09 +0000

    Added the predict method in ResultActivity to predict a price. It only works with hardcoded values. Need to change inputted strings to their encoded values.

--- app/src/main/java/ie/tudublin/carml/ResultActivity.java
+++ app/src/main/java/ie/tudublin/carml/ResultActivity.java
@@ -6,9 +6,19 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import weka.classifiers.Classifier;
 import weka.classifiers.trees.RandomForest;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator;
+
+import java.io.IOException;
+import java.util.Enumeration;
 
 public class ResultActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,6 +27,7 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
     TextView manufacturer;
     TextView model;
     TextView year;
+    TextView price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,7 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         manufacturer = findViewById(R.id.result_manufacturer);
         model = findViewById(R.id.result_model);
         year = findViewById(R.id.result_year);
+        price = findViewById(R.id.result_price);
         displayResult();
     }
 
@@ -56,9 +68,48 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         manufacturer.setText(user_car_split[0]);
         model.setText(user_car_split[1]);
         year.setText(user_car_split[2]);
-        RandomForest rf = new RandomForest();
-        rf.setNumIterations(100);
-        rf.setMaxDepth(10);
-        rf.setSeed(0);
+        double prediction = predict(user_car);
+        price.setText(String.valueOf(prediction));
+
+    }
+
+    public double predict(String query) {
+        String[] query_split = query.split(",");
+        int numAttributes = 4;
+        int numInstances = 1;
+        Attribute manufacturer = new Attribute("Make");
+        Attribute model = new Attribute("Model");
+        Attribute year = new Attribute("Year");
+        Attribute price = new Attribute("MSRP");
+
+        FastVector fvAttributes = new FastVector(numAttributes);
+        fvAttributes.add(manufacturer);
+        fvAttributes.add(model);
+        fvAttributes.add(year);
+        fvAttributes.add(price);
+
+        Instances instances = new Instances("Rel", fvAttributes, numInstances);
+        instances.setClassIndex(numAttributes - 1);
+
+//        Instance user_query = new DenseInstance(numAttributes);
+//        user_query.setValue(manufacturer, Float.parseFloat(query_split[0]));
+//        user_query.setValue(model, Float.parseFloat(query_split[1]));
+//        user_query.setValue(year, Float.parseFloat(query_split[2]));
+        Instance user_query = new DenseInstance(numAttributes);
+        user_query.setValue(manufacturer, 0);
+        user_query.setValue(model, 0);
+        user_query.setValue(year, 2001);
+
+        instances.add(user_query);
+
+        try {
+            Classifier rf = (Classifier)
+                    weka.core.SerializationHelper.read(getAssets().open("carml.model"));
+            return rf.classifyInstance(instances.instance(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return 0;
     }
 }
