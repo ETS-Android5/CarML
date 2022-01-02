151ec57a08ea70934b3b0c49c0425831fa116fcc
Sean-Coll <c18332623@mytudublin.ie>
2022-01-02 15:33:17 +0000

    All the relevant details of the car entered are displayed on the Result screen.

--- app/src/main/java/ie/tudublin/carml/ResultActivity.java
+++ app/src/main/java/ie/tudublin/carml/ResultActivity.java
@@ -2,6 +2,7 @@ package ie.tudublin.carml;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -22,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Locale;
@@ -33,6 +35,12 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
     TextView manufacturer;
     TextView model;
     TextView year;
+    TextView fuel_type;
+    TextView horsepower;
+    TextView transmission;
+    TextView drivetrain;
+    TextView num_doors;
+    TextView body_type;
     TextView price;
 
     @Override
@@ -47,6 +55,12 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         manufacturer = findViewById(R.id.result_manufacturer);
         model = findViewById(R.id.result_model);
         year = findViewById(R.id.result_year);
+        fuel_type = findViewById(R.id.result_fuel_type);
+        horsepower = findViewById(R.id.result_horsepower);
+        transmission = findViewById(R.id.result_transmission);
+        drivetrain = findViewById(R.id.result_drivetrain);
+        num_doors =  findViewById(R.id.result_num_doors);
+        body_type = findViewById(R.id.result_body_type);
         price = findViewById(R.id.result_price);
         displayResult();
     }
@@ -99,7 +113,7 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         Instances instances = new Instances("Rel", attributes, numInstances);
         instances.setClassIndex(numAttributes - 1);
 
-        double[] encodedVals = getEncodedVals(query_split[0], query_split[1]);
+        double[] encodedVals = getEncodedVals(query);
 
         Instance user_query = new DenseInstance(numAttributes);
         user_query.setValue(manufacturer, encodedVals[0]);
@@ -119,11 +133,16 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         return 0;
     }
 
-    public double[] getEncodedVals(String man, String mod) {
+    public double[] getEncodedVals(String query) {
+        // query is manufacturer,model,year
         double[] result = {0,0};
+        String[] query_split = query.split(",");
+        String man = query_split[0];
+        String mod = query_split[1];
+        String year = query_split[2];
 
         // Get InputStream for String data
-        InputStream sInStream = getResources().openRawResource(R.raw.sorted_data);
+        InputStream sInStream = getResources().openRawResource(R.raw.descriptive_data);
         // Create Buffered Reader for String data stream
         BufferedReader sReader = new BufferedReader(
                 new InputStreamReader(sInStream, StandardCharsets.UTF_8));
@@ -150,6 +169,8 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
                     String[] eSplits = eRow.split(",");
                     result[0] = Double.parseDouble(eSplits[0]);
                     result[1] = Double.parseDouble(eSplits[1]);
+                    String[] car_details = Arrays.copyOfRange(splits, 3, splits.length);
+                    displayCarDetails(car_details);
                     break;
                 }
                 else {
@@ -162,5 +183,23 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         }
 
         return result;
+    }
+
+    public void displayCarDetails(String[] details) {
+        // details is Engine Fuel Type,Engine HP,Transmission Type,
+        // Driven_Wheels,Number of Doors,Vehicle Style,MSRP
+        fuel_type.setText(formatString(details[0]));
+        horsepower.setText(formatString(details[1]));
+        transmission.setText(formatString(details[2]));
+        drivetrain.setText(formatString(details[3]));
+        num_doors.setText(formatString(details[4]));
+        body_type.setText(formatString(details[5]));
+    }
+
+    // Take a string and format is so only the first character is uppercase
+    // and the rest are lower case
+    public String formatString(String s) {
+        s = s.toLowerCase();
+        return (s.substring(0,1).toUpperCase() + s.substring(1));
     }
 }
