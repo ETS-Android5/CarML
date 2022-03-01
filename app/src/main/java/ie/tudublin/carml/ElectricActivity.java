736bb1fb4a999382b8d18144ec6d770e03ed39b6
Sean-Coll <c18332623@mytudublin.ie>
2022-03-01 21:49:02 +0000

    The first link works as intended.

--- app/src/main/java/ie/tudublin/carml/ElectricActivity.java
+++ app/src/main/java/ie/tudublin/carml/ElectricActivity.java
@@ -1,15 +1,19 @@
 package ie.tudublin.carml;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 
 public class ElectricActivity extends Activity implements View.OnClickListener {
 
     ImageButton back_arrow;
+    RelativeLayout easyGo;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,11 +30,18 @@ public class ElectricActivity extends Activity implements View.OnClickListener {
                 finish();
                 break;
             }
+            case(R.id.easygo_layout): {
+                Intent easyGoIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://easygo.ie/charging-at-home/"));
+                startActivity(easyGoIntent);
+                break;
+            }
         }
     }
 
     public void setUpViews() {
         back_arrow = findViewById(R.id.back_arrow);
         back_arrow.setOnClickListener(this);
+        easyGo = findViewById(R.id.easygo_layout);
+        easyGo.setOnClickListener(this);
     }
 }
